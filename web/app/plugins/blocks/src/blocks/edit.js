b8ab4900ebcf1efebf175b8cc5ad6d434aa810f3
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-05-16 19:02:32 +0300

    add blocks plugin

---
+++ web/app/plugins/blocks/src/blocks/edit.js
@@ -0,0 +1,9 @@
+import { __ } from "@wordpress/i18n";
+
+import { useBlockProps } from "@wordpress/block-editor";
+
+import "./editor.scss";
+
+export default function Edit() {
+  return <p {...useBlockProps()}>{__("Blocks – hello from the editor!", "blocks")}</p>;
+}
