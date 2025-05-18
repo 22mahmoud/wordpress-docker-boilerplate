d4dbfbfcfab8a718c70032bfce8f98ed5ada62f1
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-05-18 13:31:39 +0300

    test without entrypoint

--- docker/php-fpm/entrypoint.prod.sh
+++ docker/php-fpm/entrypoint.prod.sh
@@ -4,13 +4,13 @@ set -e
 
 echo "🏁 production entrypoint starting..."
 
-. /usr/local/bin/entrypoint.common.sh
+# . /usr/local/bin/entrypoint.common.sh
 
 # echo "🔄 Running WordPress database updates..."
 # wp core update-db
 
-echo "🧹 Flushing WordPress cache..."
-wp cache flush
+# echo "🧹 Flushing WordPress cache..."
+# wp cache flush
 
 echo "✅ Production entrypoint finished. Starting PHP-FPM..."
 exec "$@"
